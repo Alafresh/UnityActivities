d72a87d5e27e17d1068b140fa4e8fb70d95ee62c
Alafresh <51685896+Alafresh@users.noreply.github.com>
2023-12-07 17:54:26 -0500

    update final

---
+++ Assets/Script/Physics/MovimientoSimple.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoSimple : MonoBehaviour
+{
+    public float velocidad = 5f;
+    // Update is called once per frame
+    void Update()
+    {
+        float movimientoHorizontal = Input.GetAxis("Horizontal");
+        float movimientoVertical = Input.GetAxis("Vertical");
+
+        Vector3 movimiento = new Vector3();
+    }
+}
